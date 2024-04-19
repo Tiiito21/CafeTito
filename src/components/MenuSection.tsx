c5b2e89d13fe27afff6dfeb80c6370ee6f9a5718
Tiiito21 <alejandroacba03@gmail.com>
2024-04-19 23:58:14 +0200

    any type added to setIsOpen

--- src/components/MenuSection.tsx
+++ src/components/MenuSection.tsx
@@ -9,13 +9,13 @@ const MenuSection = ({ section, dishes }: any) => {
 
   const [isOpen, setIsOpen] = useState<string>('');
 
-  const toggleOpen = (section:String) => {
+  const toggleOpen = (section:any) => {
     setIsOpen(section);
   };
 
   return (
     Object.entries(carta).map(([section, dishes]) => (
-      <section key={section} className="mt-2 w-3/4 flex flex-col items-center cursor-pointer" onClick={() => toggleOpen(section)}>
+      <section key={section} className="mt-2 w-[90%] md:w-3/4 flex flex-col items-center cursor-pointer" onClick={() => toggleOpen(section)}>
         <h2 className={`py-2 w-full text-center text-2xl md:text-4xl font-serif font-black border-2 rounded-xl transition-colors duration-500 border-secondary ${isOpen==section ? 'bg-secondary text-background' : 'bg-background text-secondary' }`} >{section}</h2>
         
         {
@@ -23,7 +23,7 @@ const MenuSection = ({ section, dishes }: any) => {
             ${isOpen==section ? '' : 'scale-0 h-0'}`}>
           {
             dishes.map((dish: any) => (   
-              <li key={dish.nombre} className="text-lg md:text-xl text-center font-serif font-bold text-secondary flex justify-between">
+              <li key={dish.nombre} className="text-md md:text-xl text-center font-serif font-bold text-secondary flex justify-between">
                 <p className="overflow-hidden whitespace-nowrap" >{dish.nombre}</p>
                 <p >{dish.precio}€</p>
               </li>
